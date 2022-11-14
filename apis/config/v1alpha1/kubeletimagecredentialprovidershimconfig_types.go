8501ba7b2022d0819e913cbb7eb0cd4defe139d5
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-14 16:43:18 +0000

    feat: Embed kubelet credential provider config in shim config kind

--- apis/config/v1alpha1/kubeletimagecredentialprovidershimconfig_types.go
+++ apis/config/v1alpha1/kubeletimagecredentialprovidershimconfig_types.go
@@ -8,6 +8,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubelet/config/v1beta1"
 )
 
 //nolint:gochecknoinits // init is the convention for registering API types.
@@ -25,6 +26,12 @@ type KubeletImageCredentialProviderShimConfig struct {
 	// Mirror is the optional mirror configuration.
 	// +optional
 	Mirror *MirrorConfig `json:"mirror,omitempty"`
+
+	// CredentialProviders holds the configuration for the kubelet credential providers. Embeds the
+	// `CredentialProviderConfig` kind from kuelet config API - see
+	// https://github.com/kubernetes/kubelet/blob/v0.25.4/config/v1beta1/types.go#L921 for info.
+	// +optional
+	CredentialProviders *v1beta1.CredentialProviderConfig `json:"credentialProviders,omitempty"`
 }
 
 // MirrorCredentialsStrategy specifies how to handle mirror registry credentials.
@@ -51,27 +58,8 @@ type MirrorConfig struct {
 	// Endpoint is the registry endpoint to use for the mirror. The endpoint must be a valid url
 	// with host specified. The scheme, host and path from the endpoint URL will be used.
 	Endpoint string `json:"endpoint"`
-	// AuthConfig is the optional static authentication configuration to use for the mirror. If
-	// unspecified and the mirror endpoint is a known registry provider, the appropriate kubelet
-	// image credential provider plugin will be called to retried the auth config to use.
-	// +optional
-	AuthConfig *AuthConfig `json:"authConfig,omitempty"`
 	// CredentialStrategy specifies what strategy to employ when returning registry credentials.
 	// Defaults to `MirrorCredentialsOnly`.
 	// +optional
 	MirrorCredentialsStrategy MirrorCredentialsStrategy `json:"credentialsStrategy"`
-}
-
-// AuthConfig is duplicated from upstream in order to keep package dependencies local.
-// AuthConfig contains authentication information for a container registry.
-// Only username/password based authentication is supported today, but more authentication
-// mechanisms may be added in the future.
-type AuthConfig struct {
-	// username is the username used for authenticating to the container registry.
-	// An empty username is valid.
-	Username string `json:"username"`
-
-	// Password is the password used for authenticating to the container registry.
-	// An empty password is valid.
-	Password string `json:"password"`
 }
--- go.mod
+++ go.mod
@@ -53,6 +53,8 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.25.3 // indirect
+	k8s.io/component-base v0.25.3 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
