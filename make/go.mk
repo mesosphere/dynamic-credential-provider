1260b2b67899dbfb48b3128c21d040a9455036df
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2022-11-30 14:52:55 -0500

    ci: fix e2e-test in Darwin by always building with GOOS=linux

--- make/go.mk
+++ make/go.mk
@@ -81,7 +81,7 @@ E2E_FLAKE_ATTEMPTS ?= 1
 e2e-test: ## Runs e2e tests
 e2e-test: install-tool.golang install-tool.ginkgo
 ifneq ($(SKIP_BUILD),true)
-e2e-test: build-snapshot
+	$(MAKE) GOOS=linux build-snapshot
 endif
 	$(info $(M) running e2e tests$(if $(E2E_LABEL), labelled "$(E2E_LABEL)")$(if $(E2E_FOCUS), matching "$(E2E_FOCUS)"))
 	ginkgo run \
