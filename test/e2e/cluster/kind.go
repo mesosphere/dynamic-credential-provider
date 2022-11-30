693c2e9aa7e4be61d9e2e9ee55b8d08a8d404dc0
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-30 14:14:50 +0000

    test: Add initial e2e test (#33)

---
+++ test/e2e/cluster/kind.go
@@ -0,0 +1,162 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/pkg/namesgenerator"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/kind/pkg/cluster"
+
+	"github.com/mesosphere/kubelet-image-credential-provider-shim/test/e2e/seedrng"
+)
+
+// kindCluster represents a KinD cluster.
+type kindCluster struct {
+	name     string
+	provider *cluster.Provider
+
+	restConfig    *rest.Config
+	client        kubernetes.Interface
+	dynamicClient dynamic.Interface
+}
+
+// NewKinDCluster creates a KinD cluster and returns a Cluster ready to be used.
+//
+//nolint:revive // Complex function is ok in this test file.
+func NewKinDCluster(
+	ctx context.Context,
+	providerOpts []cluster.ProviderOption,
+	createOpts []cluster.CreateOption,
+) (Cluster, error) {
+	seedrng.EnsureSeeded()
+
+	name := strings.ReplaceAll(namesgenerator.GetRandomName(0), "_", "-")
+
+	provider := cluster.NewProvider(providerOpts...)
+
+	// Do not export kubeconfig to file by default, makes cleanup easier. This can be overridden by using create option
+	// to configure this via `cluster.CreateWithKubeconfigPath` when calling `NewKindCluster`.
+	tempDir, err := os.MkdirTemp("", fmt.Sprintf("%s-kubeconfig-*", name))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp dir: %w", err)
+	}
+	defer os.RemoveAll(tempDir)
+	tempKubeconfig := filepath.Join(tempDir, "kubeconfig")
+	mergedCreateOpts := []cluster.CreateOption{cluster.CreateWithKubeconfigPath(tempKubeconfig)}
+	mergedCreateOpts = append(mergedCreateOpts, createOpts...)
+
+	kindClusterErr := make(chan error, 1)
+	go func() {
+		kindClusterErr <- provider.Create(name, mergedCreateOpts...)
+	}()
+
+	select {
+	case <-ctx.Done():
+		if err := provider.Delete(name, tempKubeconfig); err != nil {
+			return nil, fmt.Errorf("failed to delete KinD cluster after spec timeout: %w", err)
+		}
+		return nil, nil
+	case err := <-kindClusterErr:
+		if err != nil {
+			return nil, fmt.Errorf("failed to create KinD cluster: %w", err)
+		}
+	}
+
+	const warningDeleteKinD = "WARNING: failed to delete KinD cluster: %v"
+	kubeconfig, err := provider.KubeConfig(name, false)
+	if err != nil {
+		if deleteErr := provider.Delete(name, tempKubeconfig); deleteErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningDeleteKinD, deleteErr)
+		}
+		return nil, fmt.Errorf("failed to retrieve kubeconfig: %w", err)
+	}
+
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig([]byte(kubeconfig))
+	if err != nil {
+		if deleteErr := provider.Delete(name, ""); deleteErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningDeleteKinD, deleteErr)
+		}
+		return nil, fmt.Errorf("failed to build REST config from kubeconfig: %w", err)
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		if deleteErr := provider.Delete(name, ""); deleteErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningDeleteKinD, deleteErr)
+		}
+		return nil, fmt.Errorf("failed to create Kubernetes client: %w", err)
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		if deleteErr := provider.Delete(name, ""); deleteErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningDeleteKinD, deleteErr)
+		}
+		return nil, fmt.Errorf("failed to create dynamic Kubernetes client: %w", err)
+	}
+
+	err = wait.PollInfiniteWithContext(ctx, time.Second*1, func(ctx context.Context) (bool, error) {
+		_, getSAErr := kubeClient.CoreV1().ServiceAccounts(metav1.NamespaceDefault).
+			Get(ctx, "default", metav1.GetOptions{})
+		if getSAErr == nil {
+			return true, nil
+		}
+		if errors.IsNotFound(getSAErr) {
+			return false, nil
+		}
+		return false, getSAErr
+	})
+	if err != nil {
+		if err := provider.Delete(name, ""); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningDeleteKinD, err)
+		}
+		return nil, fmt.Errorf("failed to wait for default service account to exist: %w", err)
+	}
+
+	return &kindCluster{
+		name:          name,
+		provider:      provider,
+		restConfig:    restConfig,
+		client:        kubeClient,
+		dynamicClient: dynamicClient,
+	}, nil
+}
+
+func (c *kindCluster) Delete(context.Context) error {
+	return c.provider.Delete(c.name, "")
+}
+
+func (c *kindCluster) Client() kubernetes.Interface {
+	return c.client
+}
+
+func (c *kindCluster) DynamicClient() dynamic.Interface {
+	return c.dynamicClient
+}
+
+func (c *kindCluster) ControllerRuntimeClient(opts client.Options) (client.Client, error) {
+	crClient, err := client.New(c.restConfig, opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create controller runtime Kubernetes client: %w", err)
+	}
+	return crClient, nil
+}
+
+func (c *kindCluster) RESTConfig() *rest.Config {
+	return c.restConfig
+}
---
+++ test/e2e/registry/registry.go
@@ -0,0 +1,236 @@
+// Copyright 2022 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package registry
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/pkg/namesgenerator"
+	"github.com/docker/go-connections/nat"
+	"github.com/foomo/htpasswd"
+	"github.com/sethvargo/go-password/password"
+
+	"github.com/mesosphere/kubelet-image-credential-provider-shim/test/e2e/docker"
+	"github.com/mesosphere/kubelet-image-credential-provider-shim/test/e2e/seedrng"
+	"github.com/mesosphere/kubelet-image-credential-provider-shim/test/e2e/tls"
+)
+
+type registryOptions struct {
+	image         string
+	dockerNetwork string
+}
+
+type Opt func(*registryOptions)
+
+func WithImage(image string) Opt {
+	return func(ro *registryOptions) { ro.image = image }
+}
+
+func WithDockerNetwork(network string) Opt {
+	return func(ro *registryOptions) { ro.dockerNetwork = network }
+}
+
+func defaultRegistryOptions() registryOptions {
+	return registryOptions{
+		image:         "registry:2",
+		dockerNetwork: "kind",
+	}
+}
+
+type Registry struct {
+	cleanup func(context.Context) error
+
+	username        string
+	password        string
+	address         string
+	hostPortAddress string
+	caCertFile      string
+}
+
+func (r *Registry) Username() string {
+	return r.username
+}
+
+func (r *Registry) Password() string {
+	return r.password
+}
+
+func (r *Registry) Address() string {
+	return r.address
+}
+
+func (r *Registry) HostPortAddress() string {
+	return r.hostPortAddress
+}
+
+func (r *Registry) CACertFile() string {
+	return r.caCertFile
+}
+
+func (r *Registry) Delete(ctx context.Context) error {
+	if err := r.cleanup(ctx); err != nil {
+		return fmt.Errorf("failed to delete Docker registry container: %w", err)
+	}
+	return nil
+}
+
+//nolint:revive // Complex function for test is OK.
+func NewRegistry(ctx context.Context, opts ...Opt) (*Registry, error) {
+	seedrng.EnsureSeeded()
+
+	rOpt := defaultRegistryOptions()
+	for _, o := range opts {
+		o(&rOpt)
+	}
+
+	containerName := strings.ReplaceAll(namesgenerator.GetRandomName(0), "_", "-")
+
+	tlsCertsDir, cleanupTLSCerts, err := tls.GenerateCertificates(containerName)
+	if err != nil {
+		return nil, err
+	}
+
+	htpasswdFile, username, passwd, cleanupHtpasswd, err := createHtpasswdFile()
+	if err != nil {
+		if cleanupErr := cleanupTLSCerts(); cleanupErr != nil {
+			_, _ = fmt.Fprintf(
+				os.Stderr,
+				"WARNING: failed to cleanup TLS certificates dir: %v",
+				cleanupErr,
+			)
+		}
+		return nil, err
+	}
+
+	containerCfg := container.Config{
+		Image:        rOpt.image,
+		ExposedPorts: nat.PortSet{nat.Port("5000"): struct{}{}},
+		Env: []string{
+			"REGISTRY_AUTH=htpasswd",
+			"REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm",
+			"REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd",
+			"REGISTRY_HTTP_TLS_CERTIFICATE=/certs/tls.crt",
+			"REGISTRY_HTTP_TLS_KEY=/certs/tls.key",
+		},
+	}
+	hostCfg := container.HostConfig{
+		AutoRemove:   true,
+		NetworkMode:  container.NetworkMode(rOpt.dockerNetwork),
+		PortBindings: nat.PortMap{nat.Port("5000"): []nat.PortBinding{{HostIP: "127.0.0.1"}}},
+		Mounts: []mount.Mount{{
+			Type:     mount.TypeBind,
+			Source:   htpasswdFile,
+			Target:   "/auth/htpasswd",
+			ReadOnly: true,
+		}, {
+			Type:     mount.TypeBind,
+			Source:   tlsCertsDir,
+			Target:   "/certs",
+			ReadOnly: true,
+		}},
+	}
+
+	const (
+		warningTLSDir      = "WARNING: failed to cleanup TLS certificates dir: %v"
+		warningHtpasswdDir = "WARNING: failed to cleanup htpasswd dir: %v"
+	)
+
+	containerInspect, cleanupContainer, err := docker.RunContainerInBackground(
+		ctx,
+		containerName,
+		&containerCfg,
+		&hostCfg,
+	)
+	if err != nil {
+		if cleanupErr := cleanupTLSCerts(); cleanupErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningTLSDir, cleanupErr)
+		}
+		if cleanupErr := cleanupHtpasswd(); cleanupErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningHtpasswdDir, cleanupErr)
+		}
+		return nil, fmt.Errorf("failed to start Docker registry container: %w", err)
+	}
+
+	publishedPort, ok := containerInspect.NetworkSettings.Ports[nat.Port("5000/tcp")]
+	if !ok {
+		if cleanupErr := cleanupTLSCerts(); cleanupErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningTLSDir, cleanupErr)
+		}
+		if cleanupErr := cleanupHtpasswd(); cleanupErr != nil {
+			_, _ = fmt.Fprintf(os.Stderr, warningHtpasswdDir, cleanupErr)
+		}
+		if cleanupErr := cleanupContainer(ctx); cleanupErr != nil {
+			_, _ = fmt.Fprintf(
+				os.Stderr,
+				"WARNING: failed to delete Docker registry container: %v",
+				cleanupErr,
+			)
+		}
+		return nil, errors.New("failed to discover Docker registry port")
+	}
+
+	r := &Registry{
+		username:        username,
+		password:        passwd,
+		address:         net.JoinHostPort(containerName, "5000"),
+		hostPortAddress: net.JoinHostPort(publishedPort[0].HostIP, publishedPort[0].HostPort),
+		caCertFile:      filepath.Join(tlsCertsDir, "ca.crt"),
+		cleanup: func(ctx context.Context) error {
+			if cleanupErr := cleanupTLSCerts(); cleanupErr != nil {
+				_, _ = fmt.Fprintf(os.Stderr, warningTLSDir, cleanupErr)
+			}
+			if cleanupErr := cleanupHtpasswd(); cleanupErr != nil {
+				_, _ = fmt.Fprintf(os.Stderr, warningHtpasswdDir, cleanupErr)
+			}
+			return cleanupContainer(ctx)
+		},
+	}
+
+	return r, nil
+}
+
+//nolint:revive // 5 return values is OK for this test function.
+func createHtpasswdFile() (htpasswdFile, username, passwd string, cleanup func() error, err error) {
+	dir, err := os.MkdirTemp("", "registry-auth-*")
+	if err != nil {
+		return "", "", "", nil, fmt.Errorf(
+			"failed to create temporary directory for registry auth: %w",
+			err,
+		)
+	}
+	cleanup = func() error { return os.RemoveAll(dir) }
+
+	f := filepath.Join(dir, "htpasswd")
+	username = namesgenerator.GetRandomName(0)
+	passwd, err = password.Generate(
+		32,
+		8,
+		8,
+		false,
+		false,
+	) //nolint:revive // Constants for password generation.
+	if err != nil {
+		_ = cleanup()
+		return "", "", "", nil, fmt.Errorf("failed to generate password for registry auth: %w", err)
+	}
+
+	err = htpasswd.SetPassword(f, username, passwd, htpasswd.HashBCrypt)
+	if err != nil {
+		_ = cleanup()
+		return "", "", "", nil, fmt.Errorf(
+			"failed to write password to htpasswd file for registry auth: %w",
+			err,
+		)
+	}
+
+	return f, username, passwd, cleanup, nil
+}
