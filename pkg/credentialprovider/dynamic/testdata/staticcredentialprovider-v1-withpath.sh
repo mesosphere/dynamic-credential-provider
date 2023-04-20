0736b8401816cbb9ed350d190cbac86391024697
Jimmi Dyson <jimmidyson@gmail.com>
2023-04-20 11:34:19 +0100

    test: Add test for testing mirror repo with path prefix (#119)

---
+++ pkg/credentialprovider/dynamic/testdata/staticcredentialprovider-v1-withpath.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 D2iQ, Inc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+echo >&2 "Received Request: "
+echo >&2 "$(</dev/stdin)"
+
+echo '{
+  "kind":"CredentialProviderResponse",
+  "apiVersion":"credentialprovider.kubelet.k8s.io/v1",
+  "cacheKeyType":"Image",
+  "cacheDuration":"5s",
+  "auth":{
+    "*.v1withpath/apath": {"username":"v1withpath/apathuser","password":"v1withpath/apathpassword"}
+  }
+}'
